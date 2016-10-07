eb8c1aecc27a43cb0bd00c29e603573d8e9c3ba0
Dídac Sabatés <sabatesduran@gmail.com>
2016-10-07 18:36:18 +0200

    Typo error

--- provision/system.sh
+++ provision/system.sh
@@ -9,7 +9,7 @@ echo "====> Update package list"
 apt-get update -y
 apt-get dist-upgrade -y
 
-echo "====> Italling required packages"
+echo "====> Installing required packages"
 apt-get -y install build-essential software-properties-common python-software-properties
 apt-get -y install vim curl git expect sshpass rsync unzip curl wget jq imagemagick dos2unix
 echo ""
