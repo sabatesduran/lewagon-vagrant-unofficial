d21cb2eac483a691168a684de5d062e37c9aa1a5
Dídac Sabatés <sabatesduran@gmail.com>
2016-10-09 13:42:22 +0200

    Corrections on ruby install

--- provision/ruby.sh
+++ provision/ruby.sh
@@ -9,18 +9,18 @@ sudo apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev libr
 
 # Cloning repositories
 git clone https://github.com/rbenv/rbenv.git /usr/local/opt/rbenv
-echo 'export PATH="/usr/local/opt/rbenv/bin:$PATH"' >> /home/ubuntu/.bash_profile
-echo 'eval "$(rbenv init -)"' >> /home/ubuntu/.bash_profile
+echo 'export PATH="/usr/local/opt/rbenv/bin:$PATH"' >> ~/.bash_profile
+echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
 
 git clone https://github.com/rbenv/ruby-build.git /usr/local/opt/rbenv/plugins/ruby-build
-echo 'export PATH="/usr/local/opt/rbenv/plugins/ruby-build/bin:$PATH"' >> /home/ubuntu/.bash_profile
-su ubuntu -c 'source /home/ubuntu/.bash_profile'
+echo 'export PATH="/usr/local/opt/rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bash_profile
+source ~/.bash_profile
 
 # Install Ruby required version
-su ubuntu -c 'rbenv install $RUBY_VERSION'
+rbenv install $RUBY_VERSION
 
 # Establish ruby default version
-su ubuntu -c 'rbenv global $RUBY_VERSION'
+rbenv global $RUBY_VERSION
 
 # Gem installs
-su ubuntu -c 'gem install bundler rspec rubocop pry pry-byebug hub colored rails'
+gem install bundler rspec rubocop pry pry-byebug hub colored tty-prompt nokogiri rails
